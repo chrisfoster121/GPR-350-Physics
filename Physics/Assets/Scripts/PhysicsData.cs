ca2bbe298dd017d6d54d26fa16d2f74a6002053e
christopher.foster <christopher.foster@mymail.champlain.edu>
2020-11-05 17:50:28 -0500

    starting physics data and physics entity code

---
+++ Physics/Assets/Scripts/PhysicsData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+[Serializable]
+struct PhysicsData
+{
+    //Vector2 pos;
+    Vector2 vel;
+    Vector2 acc;
+    Vector2 accumulatedForces;
+    float facing ;
+    float rotVel;
+    float rotAccel;
+    float dampingConst;
+    bool ignoreForces;
+    bool isEffectedByGravity;
+
+    public void MakeDefault()
+    {
+        //pos = Vector2.zero;
+        vel = Vector2.zero;
+        acc = Vector2.zero;
+        accumulatedForces = Vector2.zero;
+        facing = 0.0f;
+        rotVel = 0.0f;
+        rotAccel = 0.0f;
+        dampingConst = 0.0f;
+        ignoreForces = false;
+        isEffectedByGravity = true;
+    }
+}
---
+++ Physics/Assets/Scripts/PhysicsEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsEntity : MonoBehaviour
+{
+
+    PhysicsData physicsData;
+    // Start is called before the first frame update
+    void Start()
+    {
+        physicsData.MakeDefault();
+    }
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        //PhysicsEngin.Integrate(physicsData);
+    }
+}
