d3181c5114e2fd0922f71beddc145894181e2b5f
christopher.foster <christopher.foster@mymail.champlain.edu>
2020-12-04 17:54:56 -0500

    adding particle system base code

---
+++ Physics/Assets/Scripts/Physics/ParticleSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystem : MonoBehaviour
+{
+
+    public GameObject particlePrefab;
+
+    public int partsPerSecond;
+    float timeBetween;
+    float timeElapsed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeBetween = 1 / partsPerSecond;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+       while (timeElapsed > timeBetween)
+       {
+            timeElapsed -= timeBetween;
+            //Instantiate(particlePrefab);
+
+       }
+    }
+}
